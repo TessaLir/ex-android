f15472dea8f82c33f769845662e15220b75881d2
Валентин Ветюков <Finik001@gmail.com>
2019-04-13 17:53:45 +0300

    #JSpec1-HelloWorld.Add first Layout.
    Создан первый Layout.

---
+++ Spec/JSpec1-HelloWorld/app/src/main/java/ru/vetukov/spec/java/level01/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.vetukov.spec.java.level01.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
