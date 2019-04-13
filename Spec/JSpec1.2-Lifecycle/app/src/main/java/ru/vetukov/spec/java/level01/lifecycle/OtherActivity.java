1cb2edbf350200b83cbb667fd761c2edc8f7468e
Валентин Ветюков <Finik001@gmail.com>
2019-04-13 23:05:34 +0300

    #JSpec-1.2-JSpec1.2-Lifecycle.Add OtherActivity actions.

--- Spec/JSpec1.2-Lifecycle/app/src/main/java/ru/vetukov/spec/java/level01/lifecycle/OtherActivity.java
+++ Spec/JSpec1.2-Lifecycle/app/src/main/java/ru/vetukov/spec/java/level01/lifecycle/OtherActivity.java
@@ -1,13 +1,42 @@
 package ru.vetukov.spec.java.level01.lifecycle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class OtherActivity extends AppCompatActivity {
+
+    private EditText mETEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        StringBuilder hello = new StringBuilder();
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(MainActivity.KEY_HAPPY)) {
+            hello.append(intent.getStringExtra(MainActivity.KEY_HAPPY));
+        }
+
+        mETEdit = findViewById(R.id.other_et_edit);
+
+        mETEdit.setText(hello);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent();
+
+        String result = mETEdit.getText().toString();
+        intent.putExtra(MainActivity.KEY_HAPPY, result);
+
+        setResult(RESULT_OK, intent);
+
+        super.onBackPressed();
     }
 }
