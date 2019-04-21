3c4d8e33e533d66999615877c7b339315e01ceeb
Валентин Ветюков <Finik001@gmail.com>
2019-04-21 18:24:35 +0300

    #JSB.1.07-HomeSensorsGyroscope.Add project.

---
+++ SB/JSB1.07-HomeSensorsGyroscope/app/src/main/java/ru/vetukov/java/sb/homesensorsgyroscope/MainActivity.java
@@ -0,0 +1,54 @@
+package ru.vetukov.java.sb.homesensorsgyroscope;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VectorView mVectorView;
+
+    private SensorManager mSensorManager;
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(final SensorEvent event)
+        {
+            mVectorView.setVector(-event.values[0], event.values[1], event.values[2]);
+        }
+
+        @Override
+        public void onAccuracyChanged(final Sensor sensor, final int accuracy) {
+            // do nothing
+        }
+    };
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVectorView = (VectorView) findViewById(R.id.view_vector);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorManager.registerListener(mSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mSensorListener);
+    }
+}
