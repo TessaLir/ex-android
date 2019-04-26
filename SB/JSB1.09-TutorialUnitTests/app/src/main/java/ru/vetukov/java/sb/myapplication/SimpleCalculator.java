c5a41dfbe565f8dc06946e64b83c0300261f3303
Валентин Ветюков <Finik001@gmail.com>
2019-04-26 14:25:19 +0300

    #JSB.1.9-TutorialUnitTests.Add class with Calculate.

---
+++ SB/JSB1.09-TutorialUnitTests/app/src/main/java/ru/vetukov/java/sb/myapplication/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package ru.vetukov.java.sb.myapplication;
+
+public class SimpleCalculator {
+
+    public int add(final int a, final int b) {
+        return a + b;
+    }
+
+    public int sub(final int a, final int b) {
+        return a - b;
+    }
+
+}
