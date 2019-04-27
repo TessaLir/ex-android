e4b86b7724204e473fbca9c29dfa8445a685e73a
Валентин Ветюков <Finik001@gmail.com>
2019-04-27 20:38:00 +0300

    #JSB1.10-TutorialPropertyAnimations.Add base settings.

--- SB/JSB1.10-TutorialPropertyAnimations/app/src/main/java/ru/vetukov/java/sb/tutorialpropertyanimations/MainActivity.java
+++ SB/JSB1.10-TutorialPropertyAnimations/app/src/main/java/ru/vetukov/java/sb/tutorialpropertyanimations/MainActivity.java
@@ -2,12 +2,42 @@ package ru.vetukov.java.sb.tutorialpropertyanimations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnFirst;
+    private Button mBtnSecond;
+
+    private View mViewFirst;
+    private View mViewSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnFirst = findViewById(R.id.main_btn_first);
+        mBtnSecond = findViewById(R.id.main_btn_second);
+
+        mViewFirst = findViewById(R.id.main_view_first);
+        mViewSecond = findViewById(R.id.main_view_second);
+
+        mBtnFirst.setOnClickListener(this);
+        mBtnSecond.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_btn_first :
+
+                break;
+            case R.id.main_btn_second :
+
+                break;
+        }
     }
 }
