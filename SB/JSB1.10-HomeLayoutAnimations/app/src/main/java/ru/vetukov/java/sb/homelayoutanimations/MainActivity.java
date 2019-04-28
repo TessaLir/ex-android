66a02d0acbca15417028eae61397322a13942502
Валентин Ветюков <Finik001@gmail.com>
2019-04-28 17:22:47 +0300

    #JSB1.10-HomeLayoutAnimations. Add horizontal view.

--- SB/JSB1.10-HomeLayoutAnimations/app/src/main/java/ru/vetukov/java/sb/homelayoutanimations/MainActivity.java
+++ SB/JSB1.10-HomeLayoutAnimations/app/src/main/java/ru/vetukov/java/sb/homelayoutanimations/MainActivity.java
@@ -9,13 +9,13 @@ import android.widget.Button;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+                          implements View.OnClickListener {
 
     private Random mRandom = new Random();
 
     private Button mBtnAdd;
     private Button mBtnRemove;
-
     private ViewGroup mLvList;
 
     @Override
@@ -23,53 +23,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBtnAdd = findViewById(R.id.main_btn_add);
         mBtnRemove = findViewById(R.id.main_btn_remove);
         mLvList = findViewById(R.id.main_view_list);
-
+        mBtnAdd = findViewById(R.id.main_btn_add);
         mBtnRemove.setOnClickListener(this);
         mBtnAdd.setOnClickListener(this);
-
-
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
             case R.id.main_btn_add :
                 onClickAdd();
                 break;
             case R.id.main_btn_remove :
                 onClickkRemove();
                 break;
-
         }
     }
 
     private void onClickAdd() {
 
         final ViewGroup.LayoutParams layoutParams =
-                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100);
+                new ViewGroup.LayoutParams(100, ViewGroup.LayoutParams.MATCH_PARENT);
         final int randomColor = Color.argb(
                 0xff,
                 (int) (mRandom.nextDouble() * 255),
                 (int) (mRandom.nextDouble() * 255),
                 (int) (mRandom.nextDouble() * 255));
-
         final View view = new View(MainActivity.this);
         view.setLayoutParams(layoutParams);
         view.setBackgroundColor(randomColor);
         mLvList.addView(view);
-
     }
 
     private void onClickkRemove() {
-
         final int childCount = mLvList.getChildCount();
         if (childCount > 0) {
             mLvList.removeViewAt((int) (mRandom.nextDouble() * childCount));
         }
-
     }
 }
