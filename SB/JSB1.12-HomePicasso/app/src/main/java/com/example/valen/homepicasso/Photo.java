d1f80bfe8d90748ff3d0d56ad7c19989f9cc271c
Valentin <finik001@gmail.com>
2019-05-10 13:36:01 +0300

    #JSB1.12-HomePicasso. Add Photo class.

---
+++ SB/JSB1.12-HomePicasso/app/src/main/java/com/example/valen/homepicasso/Photo.java
@@ -0,0 +1,14 @@
+package com.example.valen.homepicasso;
+
+public class Photo {
+
+    private String imageSRC;
+
+    public Photo(String imageSRC) {
+        this.imageSRC = imageSRC;
+    }
+
+    public String getImageSRC() {
+        return imageSRC;
+    }
+}
