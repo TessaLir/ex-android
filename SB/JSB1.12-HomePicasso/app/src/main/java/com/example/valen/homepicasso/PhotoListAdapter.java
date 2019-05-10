cde324b48d0c60a8d0df0baf92072e3ed5ea8ad1
Valentin <finik001@gmail.com>
2019-05-10 13:36:25 +0300

    #JSB1.12-HomePicasso. Add Adapter.

---
+++ SB/JSB1.12-HomePicasso/app/src/main/java/com/example/valen/homepicasso/PhotoListAdapter.java
@@ -0,0 +1,55 @@
+package com.example.valen.homepicasso;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class PhotoListAdapter extends RecyclerView.Adapter<PhotoListAdapter.ViewHolder> {
+
+    private LayoutInflater mInflater;
+    private List<Photo> photos;
+
+    public PhotoListAdapter(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        mInflater = LayoutInflater.from(parent.getContext());
+        return new ViewHolder(mInflater.inflate(R.layout.view_photo_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Picasso mPicasso = Picasso.get();
+        mPicasso.cancelRequest(holder.image);
+        holder.image.setImageBitmap(null);
+        mPicasso
+                .load(photos.get(position).getImageSRC())
+                .resizeDimen(R.dimen.image_size, R.dimen.image_size)
+                .into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return photos == null ? 0 : photos.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            image = view.findViewById(R.id.item_image);
+        }
+    }
+
+}
