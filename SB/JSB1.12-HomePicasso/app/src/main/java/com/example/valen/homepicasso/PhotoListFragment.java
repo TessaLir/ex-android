e362e9519c134cc7703065baec45d9a8f0d1dd3d
Valentin <finik001@gmail.com>
2019-05-10 13:38:00 +0300

    #JSB1.12-HomePicasso. Add layouts.

--- SB/JSB1.12-HomePicasso/app/src/main/java/com/example/valen/homepicasso/PhotoListFragment.java
+++ SB/JSB1.12-HomePicasso/app/src/main/java/com/example/valen/homepicasso/PhotoListFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +38,16 @@ public class PhotoListFragment extends Fragment {
 
         PhotoListAdapter adapter = new PhotoListAdapter(getPhotos());
         recycler.setAdapter(adapter);
+
+        View header = LayoutInflater.from(getContext()).inflate(
+                R.layout.fragment_photo_info, recycler, false);
+        header.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Header",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
