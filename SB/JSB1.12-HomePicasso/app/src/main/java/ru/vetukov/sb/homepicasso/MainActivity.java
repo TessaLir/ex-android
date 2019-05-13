e298396fe01c33f300f265079f7453904b9a29d5
Валентин Ветюков <Finik001@gmail.com>
2019-05-13 11:33:24 +0300

    #JSB1.12-HomePicasso. Change MainActivity.

--- SB/JSB1.12-HomePicasso/app/src/main/java/ru/vetukov/sb/homepicasso/MainActivity.java
+++ SB/JSB1.12-HomePicasso/app/src/main/java/ru/vetukov/sb/homepicasso/MainActivity.java
@@ -7,12 +7,16 @@ import com.example.valen.homepicasso.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Наша Активити ни знает ни чего и почтини чего не умеет :*(
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager()
+        // при повороте экрана, фрагменты не перегружаются.
+        if (savedInstanceState == null)
+            getSupportFragmentManager()
                                         .beginTransaction()
                                         .replace(R.id.main_container, PhotoListFragment.getInstance())
                                         .addToBackStack(null)
