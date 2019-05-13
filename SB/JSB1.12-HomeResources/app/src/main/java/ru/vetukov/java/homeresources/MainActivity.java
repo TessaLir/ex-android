83a5ab0d9718706cc8a37780536ea3bb436f8829
Валентин Ветюков <Finik001@gmail.com>
2019-05-13 21:46:52 +0300

    #JSB1.12-HomeResources. Add menu bar.

--- SB/JSB1.12-HomeResources/app/src/main/java/ru/vetukov/java/homeresources/MainActivity.java
+++ SB/JSB1.12-HomeResources/app/src/main/java/ru/vetukov/java/homeresources/MainActivity.java
@@ -2,6 +2,10 @@ package ru.vetukov.java.homeresources;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item:
+                Log.d("!@#", "Item");
+                break;
+            case R.id.menu_hidden_item:
+                Log.d("!@#", "Hidden item");
+                break;
+            case R.id.menu_hidden_cwa:
+                Toast.makeText(this, "CWA CWA", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
