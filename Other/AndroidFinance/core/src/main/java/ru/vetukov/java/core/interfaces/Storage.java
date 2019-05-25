b5bc66dacce4b157a934b2a4611451d9ee94cc07
Валентин Ветюков <Finik001@gmail.com>
2019-05-25 14:10:13 +0300

    #JB-09. AndroidFinance.  Add some Exceptions.

--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/interfaces/Storage.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/interfaces/Storage.java
@@ -5,26 +5,29 @@ import java.util.Currency;
 import java.util.List;
 import java.util.Map;
 
+import ru.vetukov.java.core.exceptions.AmountException;
+import ru.vetukov.java.core.exceptions.CurrencyException;
+
 //TODO: изменть тип BigDecimal на готовый класс по работе с деньгами.
 public interface Storage {
 
     String getName();
 
     // Получение баланса остатка
-    Map<Currency, BigDecimal> getCurrencyAmount();              // Остаток по каждой доступной валюте в хранилище
-    BigDecimal getAmount(Currency currency);                    // Остаток во определенной валюте
-    BigDecimal getApproxAmount(Currency currency);              // Примерный остаток в переводе денег в одну валюту
+    Map<Currency, BigDecimal> getCurrencyAmount();                                                          // Остаток по каждой доступной валюте в хранилище
+    BigDecimal getAmount(Currency currency) throws CurrencyException;                                       // Остаток во определенной валюте
+    BigDecimal getApproxAmount(Currency currency) throws CurrencyException;                                 // Примерный остаток в переводе денег в одну валюту
 
     // Изменение баланса
-    void changeAmount(BigDecimal amount, Currency currency);    // Изменение баланса по определенной валюте
-    void addAmount(BigDecimal amount, Currency currency);       // Добавить сумму в валюте
-    void expenseAmount(BigDecimal amount, Currency currency);   // Отнять сумму в валюте
+    void changeAmount(BigDecimal amount, Currency currency) throws CurrencyException;                       // Изменение баланса по определенной валюте
+    void addAmount(BigDecimal amount, Currency currency) throws CurrencyException;                          // Добавить сумму в валюте
+    void expenseAmount(BigDecimal amount, Currency currency) throws CurrencyException, AmountException;     // Отнять сумму в валюте
 
     // Работа в валютой
-    void addCurrency(Currency currency);                        // Добавить новую валюту в хранилище
-    void  deleteCurrency(Currency currency);                    // Удалить валюту из хранилища
-    Currency getCurrency(String code);                          // Получить валюту по коду
-    List<Currency> getAcailableCurrencies();                    // Получить все доступные вылюты хранилища в отдельной коллекции
+    void addCurrency(Currency currency) throws CurrencyException;                                           // Добавить новую валюту в хранилище
+    void  deleteCurrency(Currency currency) throws CurrencyException;                                       // Удалить валюту из хранилища
+    Currency getCurrency(String code) throws CurrencyException;                                             // Получить валюту по коду
+    List<Currency> getAvailableCurrencies();                                                                // Получить все доступные вылюты хранилища в отдельной коллекции
 
 
 }
