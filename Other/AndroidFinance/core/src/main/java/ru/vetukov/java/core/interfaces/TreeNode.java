28adab1a04dcd9fb86692e1613622bcde384a37b
Валентин Ветюков <Finik001@gmail.com>
2019-05-25 21:40:56 +0300

    #JB-09. AndroidFinance. Change interface.

---
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/interfaces/TreeNode.java
@@ -0,0 +1,28 @@
+package ru.vetukov.java.core.interfaces;
+
+import java.util.List;
+
+// позволяет сохдать древовидную струтуру из любого набора объектов, которые реализуют интерфейс TreeNode
+// паттерн "Компоновщик" - вольная реализация
+public interface TreeNode {
+
+    String getName();
+
+    long getId();                       // Каждый эллемент дерева должен иметь свой уникальный идентификатор
+
+    TreeNode getParent();               // Получить родителя
+
+    //TODO: тут может не находиться данный Child
+    TreeNode getChild(long id);         // Получить дочерний эллемент
+
+    List<TreeNode> getChild();          // Получить лист всех дочерних эллементов
+
+    void addChild(TreeNode child);      // Добмавить дочерний эллемент
+
+    //TODO: Тут нужна проверка, не лежит ли потенциальный родитель ниже по дереву.
+    void setParent(TreeNode parent);    // назначить родителя
+
+    //TODO: тут может не находиться данный Child
+    void removeChild(TreeNode child);   // Удалить дочерний эллемент
+
+}
