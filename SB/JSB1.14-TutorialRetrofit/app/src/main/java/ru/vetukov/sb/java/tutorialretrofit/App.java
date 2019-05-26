64b403c52c74b8e0b3c93f95e979be069cb914cd
Валентин Ветюков <Finik001@gmail.com>
2019-05-26 12:24:19 +0300

    #JSB1-14. TutorialRetrofit. Create App.

---
+++ SB/JSB1.14-TutorialRetrofit/app/src/main/java/ru/vetukov/sb/java/tutorialretrofit/App.java
@@ -0,0 +1,12 @@
+package ru.vetukov.sb.java.tutorialretrofit;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        BackendModule.createInstance();
+    }
+}
