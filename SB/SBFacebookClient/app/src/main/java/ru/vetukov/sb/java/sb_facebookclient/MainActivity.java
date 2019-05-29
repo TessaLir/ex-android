91049467dd2034b4b8d6dd1cc6edbc3832fc872a
Валентин Ветюков <Finik001@gmail.com>
2019-05-29 21:57:35 +0300

    #JSB-SB-FacebookClient. Add hidden navigation bar in main class.

--- SB/SBFacebookClient/app/src/main/java/ru/vetukov/sb/java/sb_facebookclient/MainActivity.java
+++ SB/SBFacebookClient/app/src/main/java/ru/vetukov/sb/java/sb_facebookclient/MainActivity.java
@@ -1,13 +1,32 @@
 package ru.vetukov.sb.java.sb_facebookclient;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.view_drawer_layout);
+        mProgressBar = findViewById(R.id.view_progress_bar);
+
+        Toolbar toolbar = findViewById(R.id.view_toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
     }
 }
