4f2f4f7db78320b7c670a37fb2eecaec6c5e3cfe
Валентин Ветюков <Finik001@gmail.com>
2019-06-10 22:08:22 +0300

    #JB.Finance. Change OperationType in enum.

--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/enums/OperationType.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/enums/OperationType.java
@@ -1,8 +1,38 @@
-package ru.vetukov.java.core.objects;
+package ru.vetukov.java.core.enums;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
-public class OperationType {
+public enum OperationType {
+
+    INCOME(1), OUTCOME(2), TRANSFER(3), CONVERT(4); // нумерация id - как в таблице
+
+    private static Map<Integer, OperationType> map = new HashMap<Integer, OperationType>();
+
+    static {
+        for (OperationType oper : OperationType.values()) {
+            map.put(oper.getId(), oper);
+        }
+    }
+
+    private Integer id;
+
+    private OperationType(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public static OperationType getType(int id) {
+        return map.get(id);
+    }
+
+
+}
+/*public class OperationType {
 
     private String name;
     private long id;
@@ -36,3 +66,4 @@ public class OperationType {
         return Objects.hash(id);
     }
 }
+*/
--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/interfaces/Source.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/interfaces/Source.java
@@ -1,6 +1,6 @@
 package ru.vetukov.java.core.interfaces;
 
-import ru.vetukov.java.core.objects.OperationType;
+import ru.vetukov.java.core.enums.OperationType;
 
 public interface Source extends TreeNode {
 
