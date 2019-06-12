c51802aba140e5d3530a31a8c9d065fd9bfe2fde
Валентин Ветюков <Finik001@gmail.com>
2019-06-12 22:04:57 +0300

    #JB.Finance. Change logic in source.

--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/dao/impls/SourceDAOImpl.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/dao/impls/SourceDAOImpl.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -16,15 +17,12 @@ import ru.vetukov.java.core.impls.DefaultSource;
 import ru.vetukov.java.core.interfaces.Source;
 
 
-//TODO можно реализовать общий абстрактный класс и вынести туда общие методы (getAll, delete и пр.
+//TODO можно реализовать общий абстрактный класс и вынести туда общие методы (getAll, delete и пр.)
+// реализация DAO не должна заниматься лишними делами - только связь с БД, заполнение объектов
 public class SourceDAOImpl implements SourceDAO {
 
     private static final String SOURCE_TABLE = "source";
-    private List<Source> sourceList = new ArrayList<>();
-
-//    // для каждого ключа (типа операции) - своя коллекция источников (корневых элементов дерева)
-//    private Map<OperationType, List<Source>> sourceMap = new EnumMap<>(OperationType.class);// минус этого подхода в том, что придется всегда работать с 2 коллекциями
-//    private TreeUtils<Source> treeConstructor = new TreeUtils();// для каждого объекта создаем свой экземпляр TreeUtils - т.к. передается тип Generics
+    private List<Source> sourceList = new ArrayList<>();// хранит все элементы сплошным списком, без разделения по деревьям и пр.
 
 
     @Override
@@ -32,24 +30,17 @@ public class SourceDAOImpl implements SourceDAO {
         sourceList.clear();
 
         try (Statement stmt = SQLiteConnection.getConnection().createStatement();
-             ResultSet rs = stmt.executeQuery("select * from " + SOURCE_TABLE + " order by parent_id")) {
+             ResultSet rs = stmt.executeQuery("select * from " + SOURCE_TABLE + " order by parent_id");) {
 
             while (rs.next()) {
                 DefaultSource source = new DefaultSource();
-                getSource(rs, source);
+                source.setId(rs.getLong("id"));
+                source.setName(rs.getString("name"));
+                source.setParentId(rs.getLong("parent_id"));
+                source.setOperationType(OperationType.getType(rs.getInt("operation_type_id")));
                 sourceList.add(source);
-
-//                Integer operationTypeId = rs.getInt("operation_type_id"); // можно использовать тип Integer
-//                OperationType operationType = OperationType.getType(operationTypeId);
-//                source.setOperationType(operationType);// operationType устанавливаем только для корневых элементов, т.к. для дочерних автоматически устанавливается тип от родителя
-
-//                Long parentId = rs.getLong("parent_id");// тип Long, чтобы можно было проверять на null
-//                sourceMap.put(operationType, sourceList);
-//                treeConstructor.addToTree(parentId, source, sourceList);
-
             }
 
-//            fillSourceMap();// разделяем коллекцию по типам (делается один раз при инициализации)
             return sourceList;// должен содержать только корневые элементы
 
         } catch (SQLException e) {
@@ -59,37 +50,34 @@ public class SourceDAOImpl implements SourceDAO {
         return null;
     }
 
-//    private void fillSourceMap() {
-//        // в sourceMap и sourceList находятся одни и те же объекты!!
-//
-//        for (OperationType type: OperationType.values()) {
-//            // используем lambda выражение для фильтрации
-//            sourceMap.put(type, sourceList.stream().filter(s -> s.getOperationType() == type).collect(Collectors.toList()));
-//        }
-//
-//    }
-
-
     @Override
     public Source get(long id) {
-        try ( PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("select * from " + SOURCE_TABLE + " where id=?")) {
+
+        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("select * from " + SOURCE_TABLE + " where id=?");) {
+
             stmt.setLong(1, id);
 
-            try (ResultSet rs = stmt.executeQuery()) {
+            try (ResultSet rs = stmt.executeQuery();) {
                 DefaultSource source = null;
 
                 if (rs.next()) {
                     source = new DefaultSource();
-                    getSource(rs, source);
+                    source.setId(rs.getLong("id"));
+                    source.setName(rs.getString("name"));
+                    source.setParentId(rs.getLong("parent_id"));
+                    source.setOperationType(OperationType.getType(rs.getInt("operation_type_id")));
                 }
+
                 return source;
             }
 
         } catch (SQLException e) {
             Logger.getLogger(SourceDAOImpl.class.getName()).log(Level.SEVERE, null, e);
         }
+
         return null;
     }
+
 
     @Override
     public boolean update(Source source) {
@@ -131,35 +119,67 @@ public class SourceDAOImpl implements SourceDAO {
     }
 
     @Override
+    // добавляет объект в БД и присваивает ему сгенерированный id
+    public boolean add(Source source) {
+        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("insert into " + SOURCE_TABLE + "(name, parent_id, operation_type_id) values(?,?,?)", Statement.RETURN_GENERATED_KEYS);) {
+
+            stmt.setString(1, source.getName());
+
+            if (source.hasParent()){
+                stmt.setLong(2, source.getParent().getId());
+            }else{
+                stmt.setNull(2, Types.BIGINT);
+            }
+
+            stmt.setLong(3, source.getOperationType().getId());
+
+            if (stmt.executeUpdate() == 1) {// если объект добавился нормально
+                try (ResultSet rs = stmt.getGeneratedKeys()) {// получаем id вставленной записи
+
+                    if (rs.next()) {
+                        source.setId(rs.getLong(1));// не забываем просвоить новый id в объект
+                    }
+
+                    return true;
+                }
+
+            }
+
+        } catch (SQLException e) {
+            Logger.getLogger(SourceDAOImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        return false;
+    }
+
+
+    @Override
     public List<Source> getList(OperationType operationType) {
         sourceList.clear();
 
-        try ( PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("select * from " + SOURCE_TABLE + " where operation_type_id=?") ) {
+        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("select * from " + SOURCE_TABLE + " where operation_type_id=?");) {
+
             stmt.setLong(1, operationType.getId());
 
-            try ( ResultSet rs = stmt.executeQuery() ) {
+            try (ResultSet rs = stmt.executeQuery();) {
                 DefaultSource source = null;
 
                 while (rs.next()) {
                     source = new DefaultSource();
-
-                    getSource(rs, source);
-
+                    source.setId(rs.getLong("id"));
+                    source.setName(rs.getString("name"));
+                    source.setParentId(rs.getLong("parent_id"));
+                    source.setOperationType(OperationType.getType(rs.getInt("operation_type_id")));
                     sourceList.add(source);
                 }
+
                 return sourceList;
             }
+
         } catch (SQLException e) {
             Logger.getLogger(SourceDAOImpl.class.getName()).log(Level.SEVERE, null, e);
         }
 
         return null;
-    }
-
-    private void getSource(ResultSet rs, DefaultSource source) throws SQLException {
-        source.setId(rs.getLong("id"));
-        source.setName(rs.getString("name"));
-        source.setParentId(rs.getLong("parent_id"));
-        source.setOperationType(OperationType.getType(rs.getInt("operation_type_id")));
     }
 }
