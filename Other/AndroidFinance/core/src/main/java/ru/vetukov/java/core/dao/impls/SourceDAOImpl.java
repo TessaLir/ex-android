510808f8958279f8c96dc481f3a730e99276ec6e
Валентин Ветюков <Finik001@gmail.com>
2019-06-12 20:33:56 +0300

    #JB.Finance. Refactoring code in Source DAO and decorator.

--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/dao/impls/SourceDAOImpl.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/dao/impls/SourceDAOImpl.java
@@ -5,33 +5,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import ru.vetukov.java.core.dao.interfaces.SourceDAO;
 import ru.vetukov.java.core.database.SQLiteConnection;
 import ru.vetukov.java.core.enums.OperationType;
 import ru.vetukov.java.core.impls.DefaultSource;
 import ru.vetukov.java.core.interfaces.Source;
-import ru.vetukov.java.core.utils.TreeConstructor;
 
 
 //TODO можно реализовать общий абстрактный класс и вынести туда общие методы (getAll, delete и пр.
 public class SourceDAOImpl implements SourceDAO {
 
     private static final String SOURCE_TABLE = "source";
-
     private List<Source> sourceList = new ArrayList<>();
 
-    // для каждого ключа (типа операции) - своя коллекция источников (корневых элементов дерева)
-    private Map<OperationType, List<Source>> sourceMap = new EnumMap<>(OperationType.class);// минус этого подхода в том, что придется всегда работать с 2 коллекциями
-
-    private TreeConstructor<Source> treeConstructor = new TreeConstructor();// для каждого объекта создаем свой экземпляр TreeConstructor - т.к. передается тип Generics
+//    // для каждого ключа (типа операции) - своя коллекция источников (корневых элементов дерева)
+//    private Map<OperationType, List<Source>> sourceMap = new EnumMap<>(OperationType.class);// минус этого подхода в том, что придется всегда работать с 2 коллекциями
+//    private TreeUtils<Source> treeConstructor = new TreeUtils();// для каждого объекта создаем свой экземпляр TreeUtils - т.к. передается тип Generics
 
 
     @Override
@@ -39,28 +32,24 @@ public class SourceDAOImpl implements SourceDAO {
         sourceList.clear();
 
         try (Statement stmt = SQLiteConnection.getConnection().createStatement();
-             ResultSet rs = stmt.executeQuery("select * from " + SOURCE_TABLE);) {
+             ResultSet rs = stmt.executeQuery("select * from " + SOURCE_TABLE + " order by parent_id")) {
 
             while (rs.next()) {
                 DefaultSource source = new DefaultSource();
-                source.setId(rs.getLong("id"));
-                source.setName(rs.getString("name"));
+                getSource(rs, source);
+                sourceList.add(source);
 
-                Integer operationTypeId = rs.getInt("operation_type_id"); // можно использовать тип Integer
+//                Integer operationTypeId = rs.getInt("operation_type_id"); // можно использовать тип Integer
+//                OperationType operationType = OperationType.getType(operationTypeId);
+//                source.setOperationType(operationType);// operationType устанавливаем только для корневых элементов, т.к. для дочерних автоматически устанавливается тип от родителя
 
-                OperationType operationType = OperationType.getType(operationTypeId);
-                source.setOperationType(operationType);// operationType устанавливаем только для корневых элементов, т.к. для дочерних автоматически устанавливается тип от родителя
-
-                Long parentId = rs.getLong("parent_id");// тип Long, чтобы можно было проверять на null
-
-                sourceMap.put(operationType, sourceList);
-
-                treeConstructor.addToTree(parentId, source, sourceList);
+//                Long parentId = rs.getLong("parent_id");// тип Long, чтобы можно было проверять на null
+//                sourceMap.put(operationType, sourceList);
+//                treeConstructor.addToTree(parentId, source, sourceList);
 
             }
 
-            fillSourceMap();// разделяем коллекцию по типам (делается один раз при инициализации)
-
+//            fillSourceMap();// разделяем коллекцию по типам (делается один раз при инициализации)
             return sourceList;// должен содержать только корневые элементы
 
         } catch (SQLException e) {
@@ -70,19 +59,35 @@ public class SourceDAOImpl implements SourceDAO {
         return null;
     }
 
-    private void fillSourceMap() {
-        // в sourceMap и sourceList находятся одни и те же объекты!!
-
-        for (OperationType type: OperationType.values()) {
-            // используем lambda выражение для фильтрации
-            sourceMap.put(type, sourceList.stream().filter(s -> s.getOperationType() == type).collect(Collectors.toList()));
-        }
-
-    }
+//    private void fillSourceMap() {
+//        // в sourceMap и sourceList находятся одни и те же объекты!!
+//
+//        for (OperationType type: OperationType.values()) {
+//            // используем lambda выражение для фильтрации
+//            sourceMap.put(type, sourceList.stream().filter(s -> s.getOperationType() == type).collect(Collectors.toList()));
+//        }
+//
+//    }
 
 
     @Override
     public Source get(long id) {
+        try ( PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("select * from " + SOURCE_TABLE + " where id=?")) {
+            stmt.setLong(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                DefaultSource source = null;
+
+                if (rs.next()) {
+                    source = new DefaultSource();
+                    getSource(rs, source);
+                }
+                return source;
+            }
+
+        } catch (SQLException e) {
+            Logger.getLogger(SourceDAOImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
         return null;
     }
 
@@ -127,6 +132,34 @@ public class SourceDAOImpl implements SourceDAO {
 
     @Override
     public List<Source> getList(OperationType operationType) {
-        return sourceMap.get(operationType);
+        sourceList.clear();
+
+        try ( PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("select * from " + SOURCE_TABLE + " where operation_type_id=?") ) {
+            stmt.setLong(1, operationType.getId());
+
+            try ( ResultSet rs = stmt.executeQuery() ) {
+                DefaultSource source = null;
+
+                while (rs.next()) {
+                    source = new DefaultSource();
+
+                    getSource(rs, source);
+
+                    sourceList.add(source);
+                }
+                return sourceList;
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(SourceDAOImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        return null;
+    }
+
+    private void getSource(ResultSet rs, DefaultSource source) throws SQLException {
+        source.setId(rs.getLong("id"));
+        source.setName(rs.getString("name"));
+        source.setParentId(rs.getLong("parent_id"));
+        source.setOperationType(OperationType.getType(rs.getInt("operation_type_id")));
     }
 }
--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/decorator/SourceSync.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/decorator/SourceSync.java
@@ -1,36 +1,70 @@
 package ru.vetukov.java.core.decorator;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import ru.vetukov.java.core.dao.interfaces.SourceDAO;
 import ru.vetukov.java.core.enums.OperationType;
 import ru.vetukov.java.core.interfaces.Source;
+import ru.vetukov.java.core.utils.TreeUtils;
 
 public class SourceSync implements SourceDAO {
 
-    private SourceDAO sourceDAO;
-    private List<Source> sourceList;
+    private TreeUtils<Source> treeUtils = new TreeUtils();// построитель дерева
+
+    // Все коллекции хранят ссылки на одни и те же объекты, но в разных "срезах"
+    // при удалении - удалять нужно из всех коллекций
+    private List<Source> treeList = new ArrayList<>(); // хранит деревья объектов без разделения по типам операции
+    private Map<OperationType, List<Source>> sourceMap = new EnumMap<>(OperationType.class); // деревья объектов с разделением по типам операции
+    private Map<Long, Source> identityMap = new HashMap<>(); // нет деревьев, каждый объект хранится отдельно, нужно для быстрого доступа к любому объекту по id (чтобы каждый раз не использовать перебор по всей коллекции List и не обращаться к бд)
+
+    private SourceDAO sourceDAO;// реализация слоя работы с БД
 
     public SourceSync(SourceDAO sourceDAO) {
         this.sourceDAO = sourceDAO;
+        init();
     }
 
-    @Override
-    public List<Source> getAll() {
-        if (sourceList==null){
-            sourceList = sourceDAO.getAll();
+    public void init() {
+        List<Source> sourceList = sourceDAO.getAll();// запрос в БД происходит только один раз, чтобы заполнить коллекцию sourceList
+
+        for (Source s : sourceList) {
+            identityMap.put(s.getId(), s);
+            treeUtils.addToTree(s.getParentId(), s, treeList);
         }
-        return sourceList;
+
+        // важно - сначала построить деревья, уже потом разделять по типам операции
+        fillSourceMap(treeList);// разделяем по типам операции
+
+
+    }
+
+    private void fillSourceMap(List<Source> list) {
+        for (OperationType type : OperationType.values()) {
+            // используем lambda выражение для фильтрации
+            sourceMap.put(type, list.stream().filter(s -> s.getOperationType() == type).collect(Collectors.toList()));
+        }
+
     }
 
     @Override
-    public Source get(long id) {
-        return null;
+    public List<Source> getAll() {// возвращает объекты уже в виде деревьев
+        return treeList;
     }
+
+    @Override
+    public Source get(long id) {// не делаем запрос в БД, а получаем ранее загруженный объект из коллекции
+        return identityMap.get(id);
+    }
+
 
     @Override
     public boolean update(Source source) {
-        if (sourceDAO.update(source)){
+        if (sourceDAO.update(source)) {
             return true;
         }
         return false;
@@ -39,15 +73,29 @@ public class SourceSync implements SourceDAO {
     @Override
     public boolean delete(Source source) {
         // TODO добавить нужные Exceptions
-        if (sourceDAO.delete(source)){
-            sourceList.remove(source);
+        if (sourceDAO.delete(source)) {
+            identityMap.remove(source.getId());
+            if (source.getParent()!=null) {// если удаляем дочерний элемент
+                source.getParent().remove(source);// т.к. у каждого дочернего элемента есть ссылка на родительский - можно быстро удалять элемент из дерева без поиска по всему дереву
+            }else{// если удаляем элемент, у которого нет родителей
+                sourceMap.get(source.getOperationType()).remove(source);
+                treeList.remove(source);
+            }
+
             return true;
         }
         return false;
     }
 
+
     @Override
     public List<Source> getList(OperationType operationType) {
-        return sourceDAO.getList(operationType);
+        return sourceMap.get(operationType);
+    }
+
+
+    // если понадобится напрямую получить объекты из БД - можно использовать sourceDAO
+    public SourceDAO getSourceDAO() {
+        return sourceDAO;
     }
 }
