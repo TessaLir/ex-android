3f1558f90aa1d02826f3f08d33ffb8716e4351f4
Валентин Ветюков <Finik001@gmail.com>
2019-06-12 20:34:14 +0300

    #JB.Finance. Refactoring code in Storage DAO and decorator.

--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/dao/impls/StorageDAOImpl.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/dao/impls/StorageDAOImpl.java
@@ -15,27 +15,47 @@ import ru.vetukov.java.core.dao.interfaces.StorageDAO;
 import ru.vetukov.java.core.database.SQLiteConnection;
 import ru.vetukov.java.core.impls.DefaultStorage;
 import ru.vetukov.java.core.interfaces.Storage;
-import ru.vetukov.java.core.utils.TreeConstructor;
 
 public class StorageDAOImpl implements StorageDAO {
-
-    private static final String CURRENCY_TABLE = "currency_amount";
+    private static final String CURRENCY_AMOUNT_TABLE = "currency_amount";
     private static final String STORAGE_TABLE = "storage";
 
-    private TreeConstructor<Storage> treeConstructor = new TreeConstructor();
-
     private List<Storage> storageList = new ArrayList<>();
+
+
+    @Override
+    public List<Storage> getAll() {
+        storageList.clear();
+
+        try (Statement stmt = SQLiteConnection.getConnection().createStatement();
+             ResultSet rs = stmt.executeQuery("select * from " + STORAGE_TABLE + " order by parent_id");) {
+
+            while (rs.next()) {
+                DefaultStorage storage = new DefaultStorage();
+                storage.setId(rs.getLong("id"));
+                storage.setName(rs.getString("name"));
+                storage.setParentId(rs.getLong("parent_id"));
+                storageList.add(storage);
+            }
+
+            return storageList;
+
+        } catch (SQLException e) {
+            Logger.getLogger(StorageDAOImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        return null;
+    }
 
 
     @Override
     public boolean addCurrency(Storage storage, Currency currency) {
 
         // для автоматического закрытия ресурсов
-        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("insert into " + CURRENCY_TABLE + "(currency_code, storage_id, amount) values(?,?,?)");) {
+        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("insert into " + CURRENCY_AMOUNT_TABLE + "(currency_code, storage_id) values(?,?)");) {
 
             stmt.setString(1, currency.getCurrencyCode());
             stmt.setLong(2, storage.getId());
-            stmt.setBigDecimal(3, BigDecimal.ZERO);
 
 
             if (stmt.executeUpdate() == 1) { // если была добавлена 1 запись
@@ -51,14 +71,9 @@ public class StorageDAOImpl implements StorageDAO {
     }
 
     @Override
-    public boolean updateCurrency(Storage storage, Currency currency, BigDecimal amount) {
-        return false;
-    }
-
-    @Override
     public boolean deleteCurrency(Storage storage, Currency currency) {
         // TODO реализовать - если есть ли операции по данной валюте - запрещать удаление
-        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("delete from " + CURRENCY_TABLE + " where storage_id=? and currency_code=?");) {
+        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("delete from " + CURRENCY_AMOUNT_TABLE + " where storage_id=? and currency_code=?");) {
 
             stmt.setLong(1, storage.getId());
             stmt.setString(2, currency.getCurrencyCode());
@@ -75,40 +90,50 @@ public class StorageDAOImpl implements StorageDAO {
         return false;
     }
 
-
     @Override
-    public Storage get(long id) {
-        return null;
-    }
+    public boolean updateAmount(Storage storage, Currency currency, BigDecimal amount) {
+        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("update " + CURRENCY_AMOUNT_TABLE + " set amount=? where storage_id=? and currency_code=?");) {
 
-    @Override
-    public List<Storage> getAll() {
-        storageList.clear();
+            stmt.setBigDecimal(1, amount);
+            stmt.setLong(2, storage.getId());
+            stmt.setString(3, currency.getCurrencyCode());
 
-        try (Statement stmt = SQLiteConnection.getConnection().createStatement();
-             ResultSet rs = stmt.executeQuery("select * from " + STORAGE_TABLE);) {
-
-            while (rs.next()) {
-                DefaultStorage storage = new DefaultStorage();
-                storage.setId(rs.getLong("id"));
-                storage.setName(rs.getString("name"));
-
-                long parentId = rs.getLong("parent_id");
-
-                treeConstructor.addToTree(parentId, storage, storageList);
-
+            if (stmt.executeUpdate() == 1) {
+                return true;
             }
-
-            return storageList;// в итоге storageList должен содержать только корневые элементы
 
         } catch (SQLException e) {
             Logger.getLogger(StorageDAOImpl.class.getName()).log(Level.SEVERE, null, e);
         }
 
-        return null;
+        return false;
     }
 
+    @Override
+    public Storage get(long id) {
+        try (PreparedStatement stmt = SQLiteConnection.getConnection().prepareStatement("select * from " + STORAGE_TABLE + " where id=?");) {
 
+            stmt.setLong(1, id);
+
+            try (ResultSet rs = stmt.executeQuery();){
+                DefaultStorage storage = null;
+
+                if (rs.next()){
+                    storage = new DefaultStorage();
+                    storage.setId(rs.getLong("id"));
+                    storage.setName(rs.getString("name"));
+                    storage.setParentId(rs.getLong("parent_id"));
+                }
+
+                return storage;
+            }
+
+        } catch (SQLException e) {
+            Logger.getLogger(SourceDAOImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        return null;
+    }
 
 
 
