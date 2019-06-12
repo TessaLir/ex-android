799e22736eae26451ffa9ebb745bcb551e55b4a5
Валентин Ветюков <Finik001@gmail.com>
2019-06-12 22:00:01 +0300

    #JB.Finance. Add Operation methods|classes|interfaces.

--- Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/abstracts/AbstractOperation.java
+++ Other/AndroidFinance/core/src/main/java/ru/vetukov/java/core/abstracts/AbstractOperation.java
@@ -2,29 +2,33 @@ package ru.vetukov.java.core.abstracts;
 
 import java.util.Calendar;
 
-public abstract class AbstractOperation {
+import ru.vetukov.java.core.enums.OperationType;
+import ru.vetukov.java.core.interfaces.Operation;
+
+public abstract class AbstractOperation implements Operation {
 
     private long id;
-    private Calendar dateTime;
-    private String description;
+    private Calendar dateTime; // дата и время выполнения операции (подставлять автоматически при создании, но можно будет изменять в любое время)
+    private String description; // доп. информация, которую вводит пользователь
+    private OperationType operationType;// тип операции (доход, расход, перевод, конвертация)
 
-    public AbstractOperation() {
+    public AbstractOperation(OperationType operationType) {
+        this.operationType = operationType;
     }
 
-    public AbstractOperation(long id, Calendar dateTime, String description) {
+    public AbstractOperation(long id, Calendar dateTime, String description, OperationType operationType) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
+        this.operationType = operationType;
     }
 
-    public AbstractOperation(long id) {
+    public AbstractOperation(long id, OperationType operationType) {
         this.id = id;
+        this.operationType = operationType;
     }
 
-    public AbstractOperation(Calendar dateTime, String description) {
-        this.dateTime = dateTime;
-        this.description = description;
-    }
+
 
     public long getId() {
         return id;
@@ -34,6 +38,7 @@ public abstract class AbstractOperation {
         this.id = id;
     }
 
+
     public Calendar getDateTime() {
         return dateTime;
     }
@@ -42,11 +47,20 @@ public abstract class AbstractOperation {
         this.dateTime = dateTime;
     }
 
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public OperationType getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(OperationType operationType) {
+        this.operationType = operationType;
     }
 }
